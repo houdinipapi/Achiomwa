1612dbd2cf19cedef4811d3a334e694820fa5c2c
Houdini Papi <ochiengcliff.co@gmail.com>
2024-07-19 00:09:35 +0300

    chore: Add ESLint, PostCSS, and Tailwind CSS configuration files and dependencies

---
+++ application/src/components/Navbar.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/services">Services</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  )
+}
+
+export default Navbar
