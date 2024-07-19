3e6e46ae9a4b26de8239c14a1363bedfcb2e8416
Houdini Papi <ochiengcliff.co@gmail.com>
2024-07-19 20:02:16 +0300

    chore: Update home page layout and content

--- application/src/components/HomePage.tsx
+++ application/src/components/HomePage.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 const HomePage = () => {
@@ -50,6 +51,42 @@ const HomePage = () => {
                     <br />
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus quis ipsam sit iure maxime accusantium, veritatis laboriosam dolorum culpa consectetur ullam nam libero. Adipisci itaque vero numquam tempora! Sed, numquam.
                 </p>
+            </div>
+
+            {/* View Plans */}
+
+            <div className="my-8">
+                <h1 className="text-3xl font-bold text-center mb-6">View Plans</h1>
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+
+                    <div className="p-6 bg-red-200 hover:bg-red-300 rounded-lg shadow-lg transform transition-transform duration-300 hover:scale-105">
+                        <Link href="#">
+                            <h2 className="text-2xl font-bold mb-4">Personal</h2>
+                            <p className="text-gray-700">
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem unde, recusandae tempora incidunt officiis fugiat magni veniam commodi blanditiis minus nulla itaque! Perspiciatis.     
+                            </p>
+                        </Link>
+                    </div>
+
+                    <div className="p-6 bg-purple-200 hover:bg-purple-300 rounded-lg shadow-lg transform transition-transform duration-300 hover:scale-105">
+                        <Link href="#">
+                            <h2 className="text-2xl font-bold mb-4">Business</h2>
+                            <p className="text-gray-700">
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem unde, recusandae tempora incidunt officiis fugiat magni veniam commodi blanditiis minus nulla itaque! Perspiciatis.     
+                            </p>
+                        </Link>
+                    </div>
+
+                    <div className="p-6 bg-blue-200 hover:bg-blue-300 rounded-lg shadow-lg transform transition-transform duration-300 hover:scale-105">
+                        <Link href="#">
+                            <h2 className="text-2xl font-bold mb-4">Corporate</h2>
+                            <p className="text-gray-700">
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem unde, recusandae tempora incidunt officiis fugiat magni veniam commodi blanditiis minus nulla itaque! Perspiciatis.     
+                            </p>
+                        </Link>
+                    </div>
+                    
+                </div>
             </div>
 
         </div>
